cafb76eb3ba3e070957aa2702b9a515d45d42f65
scromityiii <42040874+scromityiii@users.noreply.github.com>
2019-12-24 16:42:42 -0500

    Create level11.sh

---
+++ level11.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "******************************************************************" >> /home/level11/README.txt;
+echo "* Welcome to PolyBandit. This is a polymorphic clone of          *" >> /home/level11/README.txt;
+echo "* Overthewire.org's Bandit exercise. The object is to figure out *" >> /home/level11/README.txt;
+echo "* what the password is for the next level, then log into that    *" >> /home/level11/README.txt;
+echo "* next level's account using SSH.                                *" >> /home/level11/README.txt;
+echo "*                                                                *" >> /home/level11/README.txt;
+echo "* You are at Level 11                                            *" >> /home/level11/README.txt;
+echo "*                                                                *" >> /home/level11/README.txt;
+echo "* The password for the next level is in a file called inhere.txt *" >> /home/level11/README.txt;
+echo "*                                                                *" >> /home/level11/README.txt;
+echo "* When you get the password for the next level, log in to the    *" >> /home/level11/README.txt;
+echo "* next level with the command:                                   *" >> /home/level11/README.txt;
+echo "*         ssh level1@localhost                                   *" >> /home/level11/README.txt;
+echo "*                                                                *" >> /home/level11/README.txt;
+echo "******************************************************************" >> /home/level11/README.txt;
+echo "cat ~/README.txt" >> /home/level11/.bashrc
+
+echo $level12_pass > /home/level11/inhere.txt;
+chown level11:level11 /home/level11/inhere.txt;
