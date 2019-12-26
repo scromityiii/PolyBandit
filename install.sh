91540715f78761ad67f1de7026bdcca100473ac5
scromityiii <42040874+scromityiii@users.noreply.github.com>
2019-12-25 20:22:33 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -90,6 +90,73 @@ level30=${MD526_30:24:6};
 level31=${MD531_35:0:6};
 level32=${MD531_35:6:6};
 level33=${MD531_35:12:6};
+level34=${MD531_35:18:6};
+level35=${MD531_35:24:6};
+level36=${MD536_40:0:6};
+level37=${MD536_40:6:6};
+level38=${MD536_40:12:6};
+level39=${MD536_40:18:6};
+level40=${MD536_40:24:6};
+level41=${MD541_45:0:6};
+level42=${MD541_45:6:6};
+level43=${MD541_45:12:6};
+level44=${MD541_45:18:6};
+level45=${MD541_45:24:6};
+level46=${MD546_50:0:6};
+level47=${MD546_50:6:6};
+level48=${MD546_50:12:6};
+level49=${MD546_50:18:6};
+level50=${MD546_50:24:6};
+level51=${MD551_55:0:6};
+level52=${MD551_55:6:6};
+level53=${MD551_55:12:6};
+level54=${MD551_55:18:6};
+level55=${MD551_55:24:6};
+level56=${MD556_60:0:6};
+level57=${MD556_60:6:0};
+level58=${MD556_60:12:0};
+level59=${MD556_60:18:6};
+level60=${MD556_60:24:6};
+level61=${MD561_65:0:6};
+level62=${MD561_65:6:6};
+level63=${MD561_65:12:6};
+level64=${MD561_65:18:6};
+level65=${MD561_65:24:6};
+level66=${MD566_70:0:6};
+level67=${MD566_70:6:6};
+level68=${MD566_70:12:6};
+level69=${MD566_70:18:6};
+level70=${MD566_70:24:6};
+level71=${MD571_75:0:6};
+level72=${MD571_75:6:6};
+level73=${MD571_75:12:6};
+level74=${MD571_75:18:6};
+level75=${MD571_75:24:6};
+level76=${MD576_80:0:6};
+level77=${MD576_80:6:6};
+level78=${MD576_80:12:6};
+level79=${MD576_80:18:6};
+level80=${MD576_80:24:6};
+level81=${MD581_85:0:6};
+level82=${MD581_85:6:6};
+level83=${MD581_85:12:6};
+level84=${MD581_85:18:6};
+level85=${MD581_85:24:6};
+level86=${MD586_90:0:6};
+level87=${MD586_90:6:6};
+level88=${MD586_90:12:6};
+level89=${MD586_90:18:6};
+level90=${MD586_90:24:6};
+level91=${MD591_95:0:6};
+level92=${MD591_95:6:6};
+level93=${MD591_95:12:6};
+level94=${MD591_95:18:6};
+level95=${MD591_95:24:6};
+level96=${MD596_100:0:6};
+level97=${MD596_100:6:6};
+level98=${MD596_100:12:6};
+level99=${MD596_100:18:6};
+level100=${MD596_100:24:6};
 
 level0_pass="level0";
 level1_pass=$(echo -n $level1 | base64);
@@ -112,6 +179,86 @@ level17_pass=$(echo -n $level17 | base64);
 level18_pass=$(echo -n $level18 | base64);
 level19_pass=$(echo -n $level19 | base64);
 level20_pass=$(echo -n $level20 | base64);
+level21_pass=$(echo -n $level21 | base64);
+level22_pass=$(echo -n $level22 | base64);
+level23_pass=$(echo -n $level23 | base64);
+level24_pass=$(echo -n $level24 | base64);
+level25_pass=$(echo -n $level25 | base64);
+level26_pass=$(echo -n $level26 | base64);
+level27_pass=$(echo -n $level27 | base64);
+level28_pass=$(echo -n $level28 | base64);
+level29_pass=$(echo -n $level29 | base64);
+level30_pass=$(echo -n $level30 | base64);
+level31_pass=$(echo -n $level31 | base64);
+level32_pass=$(echo -n $level32 | base64);
+level33_pass=$(echo -n $level33 | base64);
+level34_pass=$(echo -n $level34 | base64);
+level35_pass=$(echo -n $level35 | base64);
+level36_pass=$(echo -n $level36 | base64);
+level37_pass=$(echo -n $level37 | base64);
+level38_pass=$(echo -n $level38 | base64);
+level39_pass=$(echo -n $level39 | base64);
+level40_pass=$(echo -n $level40 | base64);
+level41_pass=$(echo -n $level41 | base64);
+level42_pass=$(echo -n $level42 | base64);
+level43_pass=$(echo -n $level43 | base64);
+level44_pass=$(echo -n $level44 | base64);
+level45_pass=$(echo -n $level45 | base64);
+level46_pass=$(echo -n $level46 | base64);
+level47_pass=$(echo -n $level47 | base64);
+level48_pass=$(echo -n $level48 | base64);
+level49_pass=$(echo -n $level49 | base64);
+level50_pass=$(echo -n $level50 | base64);
+level51_pass=$(echo -n $level51 | base64);
+level52_pass=$(echo -n $level52 | base64);
+level53_pass=$(echo -n $level53 | base64);
+level54_pass=$(echo -n $level54 | base64);
+level55_pass=$(echo -n $level55 | base64);
+level56_pass=$(echo -n $level56 | base64);
+level57_pass=$(echo -n $level57 | base64);
+level58_pass=$(echo -n $level58 | base64);
+level59_pass=$(echo -n $level59 | base64);
+level60_pass=$(echo -n $level60 | base64);
+level61_pass=$(echo -n $level61 | base64);
+level62_pass=$(echo -n $level62 | base64);
+level63_pass=$(echo -n $level63 | base64);
+level64_pass=$(echo -n $level64 | base64);
+level65_pass=$(echo -n $level65 | base64);
+level66_pass=$(echo -n $level66 | base64);
+level67_pass=$(echo -n $level67 | base64);
+level68_pass=$(echo -n $level68 | base64);
+level69_pass=$(echo -n $level69 | base64);
+level70_pass=$(echo -n $level70 | base64);
+level71_pass=$(echo -n $level71 | base64);
+level72_pass=$(echo -n $level72 | base64);
+level73_pass=$(echo -n $level73 | base64);
+level74_pass=$(echo -n $level74 | base64);
+level75_pass=$(echo -n $level75 | base64);
+level76_pass=$(echo -n $level76 | base64);
+level77_pass=$(echo -n $level77 | base64);
+level78_pass=$(echo -n $level78 | base64);
+level79_pass=$(echo -n $level79 | base64);
+level80_pass=$(echo -n $level80 | base64);
+level81_pass=$(echo -n $level81 | base64);
+level82_pass=$(echo -n $level82 | base64);
+level83_pass=$(echo -n $level83 | base64);
+level84_pass=$(echo -n $level84 | base64);
+level85_pass=$(echo -n $level85 | base64);
+level86_pass=$(echo -n $level86 | base64);
+level87_pass=$(echo -n $level87 | base64);
+level88_pass=$(echo -n $level88 | base64);
+level89_pass=$(echo -n $level89 | base64);
+level90_pass=$(echo -n $level90 | base64);
+level91_pass=$(echo -n $level91 | base64);
+level92_pass=$(echo -n $level92 | base64);
+level93_pass=$(echo -n $level93 | base64);
+level94_pass=$(echo -n $level94 | base64);
+level95_pass=$(echo -n $level95 | base64);
+level96_pass=$(echo -n $level96 | base64);
+level97_pass=$(echo -n $level97 | base64);
+level98_pass=$(echo -n $level98 | base64);
+level99_pass=$(echo -n $level99 | base64);
+level100_pass=$(echo -n $level100 | base64);
 
 export level0_pass;
 export level1_pass;
@@ -134,6 +281,87 @@ export level17_pass;
 export level18_pass;
 export level19_pass;
 export level20_pass;
+export level21_pass;
+export level22_pass;
+export level23_pass;
+export level24_pass;
+export level25_pass;
+export level26_pass;
+export level27_pass;
+export level28_pass;
+export level29_pass;
+export level30_pass;
+export level31_pass;
+export level32_pass;
+export level33_pass;
+export level34_pass;
+export level35_pass;
+export level36_pass;
+export level37_pass;
+export level38_pass;
+export level39_pass;
+export level40_pass;
+export level41_pass;
+export level42_pass;
+export level43_pass;
+export level44_pass;
+export level45_pass;
+export level46_pass;
+export level47_pass;
+export level48_pass;
+export level49_pass;
+export level50_pass;
+export level51_pass;
+export level52_pass;
+export level53_pass;
+export level54_pass;
+export level55_pass;
+export level56_pass;
+export level57_pass;
+export level58_pass;
+export level59_pass;
+export level60_pass;
+export level61_pass;
+export level62_pass;
+export level63_pass;
+export level64_pass;
+export level65_pass;
+export level66_pass;
+export level67_pass;
+export level68_pass;
+export level69_pass;
+export level70_pass;
+export level71_pass;
+export level72_pass;
+export level73_pass;
+export level74_pass;
+export level75_pass;
+export level76_pass;
+export level77_pass;
+export level78_pass;
+export level79_pass;
+export level80_pass;
+export level81_pass;
+export level82_pass;
+export level83_pass;
+export level84_pass;
+export level85_pass;
+export level86_pass;
+export level87_pass;
+export level88_pass;
+export level89_pass;
+export level90_pass;
+export level91_pass;
+export level92_pass;
+export level93_pass;
+export level94_pass;
+export level95_pass;
+export level96_pass;
+export level97_pass;
+export level98_pass;
+export level99_pass;
+export level100_pass;
+
 
 useradd level0 --create-home --password "$(openssl passwd -1 $level0_pass)" --shell /bin/bash --user-group
 useradd level1 --create-home --password "$(openssl passwd -1 $level1_pass)" --shell /bin/bash --user-group
@@ -148,6 +376,102 @@ useradd level9 --create-home --password "$(openssl passwd -1 $level9_pass)" --sh
 useradd level10 --create-home --password "$(openssl passwd -1 $level10_pass)" --shell /bin/bash --user-group
 useradd level11 --create-home --password "$(openssl passwd -1 $level11_pass)" --shell /bin/bash --user-group
 useradd level12 --create-home --password "$(openssl passwd -1 $level12_pass)" --shell /bin/bash --user-group
+useradd level13 --create-home --password "$(openssl passwd -1 $level13_pass)" --shell /bin/bash --user-group
+useradd level14 --create-home --password "$(openssl passwd -1 $level14_pass)" --shell /bin/bash --user-group
+useradd level15 --create-home --password "$(openssl passwd -1 $level15_pass)" --shell /bin/bash --user-group
+useradd level16 --create-home --password "$(openssl passwd -1 $level16_pass)" --shell /bin/bash --user-group
+useradd level17 --create-home --password "$(openssl passwd -1 $level17_pass)" --shell /bin/bash --user-group
+useradd level18 --create-home --password "$(openssl passwd -1 $level18_pass)" --shell /bin/bash --user-group
+useradd level19 --create-home --password "$(openssl passwd -1 $level19_pass)" --shell /bin/bash --user-group
+useradd level20 --create-home --password "$(openssl passwd -1 $level20_pass)" --shell /bin/bash --user-group
+useradd level21 --create-home --password "$(openssl passwd -1 $level21_pass)" --shell /bin/bash --user-group
+useradd level22 --create-home --password "$(openssl passwd -1 $level22_pass)" --shell /bin/bash --user-group
+useradd level23 --create-home --password "$(openssl passwd -1 $level23_pass)" --shell /bin/bash --user-group
+useradd level24 --create-home --password "$(openssl passwd -1 $level24_pass)" --shell /bin/bash --user-group
+useradd level25 --create-home --password "$(openssl passwd -1 $level25_pass)" --shell /bin/bash --user-group
+useradd level26 --create-home --password "$(openssl passwd -1 $level26_pass)" --shell /bin/bash --user-group
+useradd level27 --create-home --password "$(openssl passwd -1 $level27_pass)" --shell /bin/bash --user-group
+useradd level28 --create-home --password "$(openssl passwd -1 $level28_pass)" --shell /bin/bash --user-group
+useradd level29 --create-home --password "$(openssl passwd -1 $level29_pass)" --shell /bin/bash --user-group
+useradd level30 --create-home --password "$(openssl passwd -1 $level30_pass)" --shell /bin/bash --user-group
+useradd level31 --create-home --password "$(openssl passwd -1 $level31_pass)" --shell /bin/bash --user-group
+useradd level32 --create-home --password "$(openssl passwd -1 $level32_pass)" --shell /bin/bash --user-group
+useradd level33 --create-home --password "$(openssl passwd -1 $level33_pass)" --shell /bin/bash --user-group
+useradd level34 --create-home --password "$(openssl passwd -1 $level34_pass)" --shell /bin/bash --user-group
+useradd level35 --create-home --password "$(openssl passwd -1 $level35_pass)" --shell /bin/bash --user-group
+useradd level36 --create-home --password "$(openssl passwd -1 $level36_pass)" --shell /bin/bash --user-group
+useradd level37 --create-home --password "$(openssl passwd -1 $level37_pass)" --shell /bin/bash --user-group
+useradd level38 --create-home --password "$(openssl passwd -1 $level38_pass)" --shell /bin/bash --user-group
+useradd level39 --create-home --password "$(openssl passwd -1 $level39_pass)" --shell /bin/bash --user-group
+useradd level40 --create-home --password "$(openssl passwd -1 $level40_pass)" --shell /bin/bash --user-group
+useradd level41 --create-home --password "$(openssl passwd -1 $level41_pass)" --shell /bin/bash --user-group
+useradd level42 --create-home --password "$(openssl passwd -1 $level42_pass)" --shell /bin/bash --user-group
+useradd level43 --create-home --password "$(openssl passwd -1 $level43_pass)" --shell /bin/bash --user-group
+useradd level44 --create-home --password "$(openssl passwd -1 $level44_pass)" --shell /bin/bash --user-group
+useradd level45 --create-home --password "$(openssl passwd -1 $level45_pass)" --shell /bin/bash --user-group
+useradd level46 --create-home --password "$(openssl passwd -1 $level46_pass)" --shell /bin/bash --user-group
+useradd level47 --create-home --password "$(openssl passwd -1 $level47_pass)" --shell /bin/bash --user-group
+useradd level48 --create-home --password "$(openssl passwd -1 $level48_pass)" --shell /bin/bash --user-group
+useradd level49 --create-home --password "$(openssl passwd -1 $level49_pass)" --shell /bin/bash --user-group
+useradd level50 --create-home --password "$(openssl passwd -1 $level50_pass)" --shell /bin/bash --user-group
+useradd level51 --create-home --password "$(openssl passwd -1 $level51_pass)" --shell /bin/bash --user-group
+useradd level52 --create-home --password "$(openssl passwd -1 $level52_pass)" --shell /bin/bash --user-group
+useradd level53 --create-home --password "$(openssl passwd -1 $level53_pass)" --shell /bin/bash --user-group
+useradd level54 --create-home --password "$(openssl passwd -1 $level54_pass)" --shell /bin/bash --user-group
+useradd level55 --create-home --password "$(openssl passwd -1 $level55_pass)" --shell /bin/bash --user-group
+useradd level56 --create-home --password "$(openssl passwd -1 $level56_pass)" --shell /bin/bash --user-group
+useradd level57 --create-home --password "$(openssl passwd -1 $level57_pass)" --shell /bin/bash --user-group
+useradd level58 --create-home --password "$(openssl passwd -1 $level58_pass)" --shell /bin/bash --user-group
+useradd level59 --create-home --password "$(openssl passwd -1 $level59_pass)" --shell /bin/bash --user-group
+useradd level60 --create-home --password "$(openssl passwd -1 $level60_pass)" --shell /bin/bash --user-group
+useradd level61 --create-home --password "$(openssl passwd -1 $level61_pass)" --shell /bin/bash --user-group
+useradd level62 --create-home --password "$(openssl passwd -1 $level62_pass)" --shell /bin/bash --user-group
+useradd level63 --create-home --password "$(openssl passwd -1 $level63_pass)" --shell /bin/bash --user-group
+useradd level64 --create-home --password "$(openssl passwd -1 $level64_pass)" --shell /bin/bash --user-group
+useradd level65 --create-home --password "$(openssl passwd -1 $level65_pass)" --shell /bin/bash --user-group
+useradd level66 --create-home --password "$(openssl passwd -1 $level66_pass)" --shell /bin/bash --user-group
+useradd level67 --create-home --password "$(openssl passwd -1 $level67_pass)" --shell /bin/bash --user-group
+useradd level68 --create-home --password "$(openssl passwd -1 $level68_pass)" --shell /bin/bash --user-group
+useradd level69 --create-home --password "$(openssl passwd -1 $level69_pass)" --shell /bin/bash --user-group
+useradd level70 --create-home --password "$(openssl passwd -1 $level70_pass)" --shell /bin/bash --user-group
+useradd level71 --create-home --password "$(openssl passwd -1 $level71_pass)" --shell /bin/bash --user-group
+useradd level72 --create-home --password "$(openssl passwd -1 $level72_pass)" --shell /bin/bash --user-group
+useradd level73 --create-home --password "$(openssl passwd -1 $level73_pass)" --shell /bin/bash --user-group
+useradd level74 --create-home --password "$(openssl passwd -1 $level74_pass)" --shell /bin/bash --user-group
+useradd level75 --create-home --password "$(openssl passwd -1 $level75_pass)" --shell /bin/bash --user-group
+useradd level76 --create-home --password "$(openssl passwd -1 $level76_pass)" --shell /bin/bash --user-group
+useradd level77 --create-home --password "$(openssl passwd -1 $level77_pass)" --shell /bin/bash --user-group
+useradd level78 --create-home --password "$(openssl passwd -1 $level78_pass)" --shell /bin/bash --user-group
+useradd level79 --create-home --password "$(openssl passwd -1 $level79_pass)" --shell /bin/bash --user-group
+useradd level80 --create-home --password "$(openssl passwd -1 $level80_pass)" --shell /bin/bash --user-group
+useradd level81 --create-home --password "$(openssl passwd -1 $level81_pass)" --shell /bin/bash --user-group
+useradd level82 --create-home --password "$(openssl passwd -1 $level82_pass)" --shell /bin/bash --user-group
+useradd level83 --create-home --password "$(openssl passwd -1 $level83_pass)" --shell /bin/bash --user-group
+useradd level84 --create-home --password "$(openssl passwd -1 $level84_pass)" --shell /bin/bash --user-group
+useradd level85 --create-home --password "$(openssl passwd -1 $level85_pass)" --shell /bin/bash --user-group
+useradd level86 --create-home --password "$(openssl passwd -1 $level86_pass)" --shell /bin/bash --user-group
+useradd level87 --create-home --password "$(openssl passwd -1 $level87_pass)" --shell /bin/bash --user-group
+useradd level88 --create-home --password "$(openssl passwd -1 $level88_pass)" --shell /bin/bash --user-group
+useradd level89 --create-home --password "$(openssl passwd -1 $level89_pass)" --shell /bin/bash --user-group
+useradd level90 --create-home --password "$(openssl passwd -1 $level90_pass)" --shell /bin/bash --user-group
+useradd level91 --create-home --password "$(openssl passwd -1 $level91_pass)" --shell /bin/bash --user-group
+useradd level92 --create-home --password "$(openssl passwd -1 $level92_pass)" --shell /bin/bash --user-group
+useradd level93 --create-home --password "$(openssl passwd -1 $level93_pass)" --shell /bin/bash --user-group
+useradd level94 --create-home --password "$(openssl passwd -1 $level94_pass)" --shell /bin/bash --user-group
+useradd level95 --create-home --password "$(openssl passwd -1 $level95_pass)" --shell /bin/bash --user-group
+useradd level96 --create-home --password "$(openssl passwd -1 $level96_pass)" --shell /bin/bash --user-group
+useradd level97 --create-home --password "$(openssl passwd -1 $level97_pass)" --shell /bin/bash --user-group
+useradd level98 --create-home --password "$(openssl passwd -1 $level98_pass)" --shell /bin/bash --user-group
+useradd level99 --create-home --password "$(openssl passwd -1 $level99_pass)" --shell /bin/bash --user-group
+useradd level100 --create-home --password "$(openssl passwd -1 $leve100_pass)" --shell /bin/bash --user-group
+
+
+
+
+
+
+
+
 
 cd PolyBandit
 ./level0.sh;
@@ -163,6 +487,96 @@ cd PolyBandit
 ./level10.sh;
 ./level11.sh;
 ./level12.sh;
+./level13.sh;
+./level14.sh;
+./level15.sh;
+./level16.sh;
+./level17.sh;
+./level18.sh;
+./level19.sh;
+./level20.sh;
+./level21.sh;
+./level22.sh;
+./level23.sh;
+./level24.sh;
+./level25.sh;
+./level26.sh;
+./level27.sh;
+./level28.sh;
+./level29.sh;
+./level30.sh;
+./level31.sh;
+./level32.sh;
+./level33.sh;
+./level34.sh;
+./level35.sh;
+./level36.sh;
+./level37.sh;
+./level38.sh;
+./level39.sh;
+./level40.sh;
+./level41.sh;
+./level42.sh;
+./level43.sh;
+./level44.sh;
+./level45.sh;
+./level46.sh;
+./level47.sh;
+./level48.sh;
+./level49.sh;
+./level50.sh;
+./level51.sh;
+./level52.sh;
+./level53.sh;
+./level54.sh;
+./level55.sh;
+./level56.sh;
+./level57.sh;
+./level58.sh;
+./level59.sh;
+./level60.sh;
+./level61.sh;
+./level62.sh;
+./level63.sh;
+./level64.sh;
+./level65.sh;
+./level66.sh;
+./level67.sh;
+./level68.sh;
+./level69.sh;
+./level70.sh;
+./level71.sh;
+./level72.sh;
+./level73.sh;
+./level74.sh;
+./level75.sh;
+./level76.sh;
+./level77.sh;
+./level78.sh;
+./level79.sh;
+./level80.sh;
+./level81.sh;
+./level82.sh;
+./level83.sh;
+./level84.sh;
+./level85.sh;
+./level86.sh;
+./level87.sh;
+./level88.sh;
+./level89.sh;
+./level90.sh;
+./level91.sh;
+./level92.sh;
+./level93.sh;
+./level94.sh;
+./level95.sh;
+./level96.sh;
+./level97.sh;
+./level98.sh;
+./level99.sh;
+./level100.sh;
+
+
 
 echo "Now starting the game. Please log in with the password 'level0'";
 
