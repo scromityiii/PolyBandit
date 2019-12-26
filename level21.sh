62079b6f08c5d26b8ebe728256fe3f8fb6334db3
scromityiii <42040874+scromityiii@users.noreply.github.com>
2019-12-26 12:10:26 -0500

    Update level21.sh

--- level21.sh
+++ level21.sh
@@ -7,7 +7,7 @@
 	echo "* what the password is for the next level, then log into that    *" >> /home/level21/README.txt;
 	echo "* next level's account using SSH.                                *" >> /home/level21/README.txt;
 	echo "*                                                                *" >> /home/level21/README.txt;
-	echo "* You are at Level 11                                            *" >> /home/level21/README.txt;
+	echo "* You are at Level 21                                            *" >> /home/level21/README.txt;
 	echo "*                                                                *" >> /home/level21/README.txt;
 	echo "* The password for the next level is in a *hidden file*          *" >> /home/level21/README.txt;
 	echo "* called inhere.txt                                              *" >> /home/level21/README.txt;
