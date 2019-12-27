7a35d5778e1fc44fad42b1f1c609edd51ba31075
scromityiii <42040874+scromityiii@users.noreply.github.com>
2019-12-27 12:18:36 -0500

    Create level68.sh

---
+++ level68.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+						
+					
+				
+			
+		
+	
+
+						# I don't think we need this - NG useradd level68_ans --create-home --password "$(openssl passwd -1 "password")" --shell /bin/bash --user-group
+						
+					
+				
+			
+		
+	
+
+						echo "******************************************************************" >> /home/level68/README.txt;
+						echo "* Welcome to PolyBandit. This is a polymorphic clone of          *" >> /home/level68/README.txt;
+						echo "* Overthewire.org's Bandit exercise. The object is to figure out *" >> /home/level68/README.txt;
+						echo "* what the password is for the next level, then log into that    *" >> /home/level68/README.txt;
+						echo "* next level's account using SSH.                                *" >> /home/level68/README.txt;
+						echo "*                                                                *" >> /home/level68/README.txt;
+						echo "* You are at Level 68                                            *" >> /home/level68/README.txt;
+						echo "*                                                                *" >> /home/level68/README.txt;
+						echo "* The password for the next level is in a file called inhere.txt *" >> /home/level68/README.txt;
+						echo "* but there is a problem when you try to log in.                 *" >> /home/level68/README.txt;
+						echo "*                                                                *" >> /home/level68/README.txt;
+						echo "* When you get the password for the next level, log in to the    *" >> /home/level68/README.txt;
+						echo "* next level with the command:                                   *" >> /home/level68/README.txt;
+						echo "*         ssh level69@localhost                                   *" >> /home/level68/README.txt;
+						echo "*                                                                *" >> /home/level68/README.txt;
+						echo "******************************************************************" >> /home/level68/README.txt;
+						echo "cat /home/level68/README.txt" >> /home/level68/.bashrc
+						
+					
+				
+			
+		
+	
+
+						# this goes in Level 69's setup sudo echo "exit" >> /home/level69/.bashrc;
+						
+					
+				
+			
+		
+	
+
+						echo $level69_pass > /home/level68/inhere.txt;
+						chown level68:level68 /home/level68/inhere.txt;
+	
+
