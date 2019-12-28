5b8fb9757c761dfb5817c3bb9d5ba31f50c548ab
scromityiii <42040874+scromityiii@users.noreply.github.com>
2019-12-27 21:27:03 -0500

    Create level101.sh

---
+++ level101.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+	
+
+	echo "******************************************************************" >> /home/level101/README.txt;
+	echo "* Welcome to PolyBandit. This is a polymorphic clone of          *" >> /home/level101/README.txt;
+	echo "* Overthewire.org's Bandit exercise. The object is to figure out *" >> /home/level101/README.txt;
+	echo "* what the password is for the next level, then log into that    *" >> /home/level101/README.txt;
+	echo "* next level's account using SSH.                                *" >> /home/level101/README.txt;
+	echo "*                                                                *" >> /home/level101/README.txt;
+	echo "* You have completed the game.                                   *" >> /home/level101/README.txt;
+	echo "*                                                                *" >> /home/level101/README.txt;
+	echo "* Thank you for your participation                               *" >> /home/level101/README.txt;
+	echo "*                                                                *" >> /home/level101/README.txt;
+	echo "*                                                                *" >> /home/level101/README.txt;
+	echo "*                                                                *" >> /home/level101/README.txt;
+	echo "*                                                                *" >> /home/level101/README.txt;
+	echo "*                                                                *" >> /home/level101/README.txt;
+	echo "*                                                                *" >> /home/level101/README.txt;
+	echo "******************************************************************" >> /home/level101/README.txt;
+	
+
+	
+
+	echo "cat /home/level101/README.txt" >> /home/level101/.bashrc
+	
+
+	
+	chown level101:level101 /home/level101/.inhere.txt;
