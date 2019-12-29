119c02db6533b53e51af3406c5b32dd68ffc0c30
scromityiii <42040874+scromityiii@users.noreply.github.com>
2019-12-29 16:06:23 -0500

    Update level97.sh

--- level97.sh
+++ level97.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
+                                                                echo "cat /home/level97/README.txt" >> /home/level97/.bashrc
+								echo "******************************************************************" >> /home/level97/README.txt;
+								echo "* Welcome to PolyBandit. This is a polymorphic clone of          *" >> /home/level97/README.txt;
+								echo "* Overthewire.org's Bandit exercise. The object is to figure out *" >> /home/level97/README.txt;
+								echo "* what the password is for the next level, then log into that    *" >> /home/level97/README.txt;
+								echo "* next level's account using SSH.                                *" >> /home/level97/README.txt;
+								echo "*                                                                *" >> /home/level97/README.txt;
+								echo "* You are at Level 87                                            *" >> /home/level97/README.txt;
+								echo "*                                                                *" >> /home/level97/README.txt;
+								echo "* Your password is in the inhere.txt file. However, it is not so *" >> /home/level97/README.txt;
+								echo "* easy to read the file.                                         *" >> /home/level97/README.txt;
+								echo "*                                                                *" >> /home/level97/README.txt;
+								echo "* When you get the password for the next level, log in to the    *" >> /home/level97/README.txt;
+								echo "* next level with the command:                                   *" >> /home/level97/README.txt;
+								echo "*         ssh level98@localhost                                  *" >> /home/level97/README.txt;
+								echo "*                                                                *" >> /home/level97/README.txt;
+								echo "******************************************************************" >> /home/level97/README.txt;
+								
 								
 							
 						
@@ -8,23 +26,8 @@
 		
 	
 
-								echo "******************************************************************" >> /home/level87/README.txt;
-								echo "* Welcome to PolyBandit. This is a polymorphic clone of          *" >> /home/level87/README.txt;
-								echo "* Overthewire.org's Bandit exercise. The object is to figure out *" >> /home/level87/README.txt;
-								echo "* what the password is for the next level, then log into that    *" >> /home/level87/README.txt;
-								echo "* next level's account using SSH.                                *" >> /home/level87/README.txt;
-								echo "*                                                                *" >> /home/level87/README.txt;
-								echo "* You are at Level 87                                            *" >> /home/level87/README.txt;
-								echo "*                                                                *" >> /home/level87/README.txt;
-								echo "* Your password is in the inhere.txt file. However, it is not so *" >> /home/level87/README.txt;
-								echo "* easy to read the file.                                         *" >> /home/level87/README.txt;
-								echo "*                                                                *" >> /home/level87/README.txt;
-								echo "* When you get the password for the next level, log in to the    *" >> /home/level87/README.txt;
-								echo "* next level with the command:                                   *" >> /home/level87/README.txt;
-								echo "*         ssh level88@localhost                                  *" >> /home/level87/README.txt;
-								echo "*                                                                *" >> /home/level87/README.txt;
-								echo "******************************************************************" >> /home/level87/README.txt;
-								echo "cat /home/level87/README.txt" >> /home/level87/.bashrc
+								echo $level98_pass > /home/level97/inhere.txt;
+								chown level97:level97 /home/level97/inhere.txt;
 								
 							
 						
@@ -34,15 +37,4 @@
 		
 	
 
-								echo $level88_pass > /home/level87/inhere.txt;
-								chown level87:level87 /home/level87/inhere.txt;
-								
-							
-						
-					
-				
-			
-		
-	
-
-								chmod 333 /home/level87/inhere.txt;
+								chmod 333 /home/level97/inhere.txt;
