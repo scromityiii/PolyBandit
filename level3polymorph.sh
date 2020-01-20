0676faa3d98fa8083791f82017e122bbaf9754da
scromityiii <42040874+scromityiii@users.noreply.github.com>
2020-01-20 15:48:37 -0500

    Update level3polymorph.sh

--- level3polymorph.sh
+++ level3polymorph.sh
@@ -479,7 +479,7 @@ changer=${MD51_5:0:1}
 		fi
 
 
-The below was commented out due to permutation
+#The below was commented out due to permutation
 #password_spot=$[RANDOM%256];
 
 declare -a LETTERS=("a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z" "A" "B" "C" "D" "E" "F" "G" "H" "I" "J" "K" "L" "M" "N" "O" "P" "Q" "R" "S" "T" "U" "V" "W" "X" "Y" "Z" "1" "2" "3" "4" "5" "6" "7" "8" "9" "0");
