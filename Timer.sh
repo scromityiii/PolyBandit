aa23e5dd696543e32660cb26675008c2dea75907
scromityiii <42040874+scromityiii@users.noreply.github.com>
2020-01-29 12:44:40 -0500

    Create Timer.sh

---
+++ Timer.sh
@@ -0,0 +1,6 @@
+
+echo "This script is a timer to be used while playing PolyBandit. When finished with the game, hold the control button and then" 
+echo "hit c to stop the timer. Please screenshot this along with the final page when you are done playing."
+date1=`date +%s`; while true; do
+                echo -ne "$(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S)\r" >> /home/level101/README.txt;
+                done
