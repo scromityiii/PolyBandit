830d7c869160312d98e265ec03bf3f574beee83f
Steve Cromity III <s.cromityiii@gmail.com>
2020-01-30 10:17:16 -0800

    adding level 5

--- level5polymorph.sh
+++ level5polymorph.sh
@@ -597,68 +597,6 @@ declare -a users=("aeijdoifj" "boidsjoi" "8327cdkjc" "h923878d" "ei4378y7" "897y
 
 declare -a LETTERS=("a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z" "A" "B" "C" "D" "E" "F" "G" "H" "I" "J" "K" "L" "M" "N" "O" "P" "Q" "R" "S" "T" "U" "V" "W" "X" "Y" "Z" "1" "2" "3" "4" "5" "6" "7" "8" "9" "0");
 
-count1=0;
-while [[ $count1 -lt $noiseamount1 ]];
-do
-  char1=$[RANDOM%61];
-  char2=$[RANDOM%61];
-  char3=$[RANDOM%61];
-  char4=$[RANDOM%61];
-  char5=$[RANDOM%61];
-  char6=$[RANDOM%61];
-  char7=$[RANDOM%61];
-  char8=$[RANDOM%61];
-
-  
-  mkdir /home/level5/$rand_dir1;
-  count2=0;
-  while [[ $count2 -lt $noiseamount2 ]];
-  do
-    char1=$[RANDOM%61];
-    char2=$[RANDOM%61];
-    char3=$[RANDOM%61];
-    char4=$[RANDOM%61];
-    char5=$[RANDOM%61];
-    char6=$[RANDOM%61];
-    char7=$[RANDOM%61];
-    char8=$[RANDOM%61];
-
-    
-    mkdir /home/level5/$rand_dir1/$rand_dir2;
-    count3=0;
-    while [[ $count3 -lt $noiseamount3 ]];
-    do
-
-      if (($count1 == $password_spot1 && $count2 == $password_spot2 && $count3 == $password_spot3));
-      then
-
-        echo $level6_pass > home/level5/$directory1/$directory2/$textfilename
-        chown level5:level5 home/level5/$directory1/$directory2/$textfilename
-        count3=$(($count3+1));
-      else
-        char1=$[RANDOM%61];
-        char2=$[RANDOM%61];
-        char3=$[RANDOM%61];
-        char4=$[RANDOM%61];
-        char5=$[RANDOM%61];
-        char6=$[RANDOM%61];
-        char7=$[RANDOM%61];
-        char8=$[RANDOM%61];
-
-
-	echo "these are not the passwords you're looking for" "$textfilenoise" > /home/level5/$rand_dir1/$rand_dir2/$rand_file.txt
-        chown level5:level5 /home/level5/$rand_dir1/$rand_dir2/$rand_file.txt
-        count3=$(($count3+1));
-      fi
-    done
-    cd ..
-    count2=$(($count2+1));
-  done
-  cd ..
-  count1=$(($count1+1));
-done
-
-
 
 echo "******************************************************************" >> /home/level5/README.txt;
 echo "* Welcome to PolyBandit. This is a polymorphic clone of          *" >> /home/level5/README.txt;
@@ -679,3 +617,73 @@ echo "*         ssh level6@localhost                                   *" >> /ho
 echo "*                                                                *" >> /home/level5/README.txt;
 echo "******************************************************************" >> /home/level5/README.txt;
 echo "cat /home/level5/README.txt" >> /home/level5/.bashrc
+
+
+
+
+
+
+
+count1=0;
+while [[ $count1 -lt $noiseamount1 ]];
+do
+  char1=$[RANDOM%61];
+  char2=$[RANDOM%61];
+  char3=$[RANDOM%61];
+  char4=$[RANDOM%61];
+  char5=$[RANDOM%61];
+  char6=$[RANDOM%61];
+  char7=$[RANDOM%61];
+  char8=$[RANDOM%61];
+
+  rand_dir11=${rand_dir1}
+  mkdir /home/level5/$rand_dir11;
+  count2=0;
+  while [[ $count2 -lt $noiseamount2 ]];
+  do
+    char1=$[RANDOM%61];
+    char2=$[RANDOM%61];
+    char3=$[RANDOM%61];
+    char4=$[RANDOM%61];
+    char5=$[RANDOM%61];
+    char6=$[RANDOM%61];
+    char7=$[RANDOM%61];
+    char8=$[RANDOM%61];
+
+    randdir22=${rand_dir2}
+    mkdir /home/level5/$rand_dir1/$rand_dir22;
+    count3=0;
+    while [[ $count3 -lt $noiseamount3 ]];
+    do
+
+      if (($count1 == $password_spot1 && $count2 == $password_spot2 && $count3 == $password_spot3));
+      then
+
+        echo $level6_pass > home/level5/$directory1/$directory2/$textfilename
+        chown level5:level5 home/level5/$directory1/$directory2/$textfilename
+        count3=$(($count3+1));
+      else
+        char1=$[RANDOM%61];
+        char2=$[RANDOM%61];
+        char3=$[RANDOM%61];
+        char4=$[RANDOM%61];
+        char5=$[RANDOM%61];
+        char6=$[RANDOM%61];
+        char7=$[RANDOM%61];
+        char8=$[RANDOM%61];
+
+       
+	echo "these are not the passwords you're looking for" "$textfilenoise" > /home/level5/$rand_dir1/$rand_dir2/$rand_file.txt
+        chown level5:level5 /home/level5/$rand_dir1/$rand_dir2/$rand_file.txt
+        count3=$(($count3+1));
+      fi
+    done
+    cd ..
+    count2=$(($count2+1));
+  done
+  cd ..
+  count1=$(($count1+1));
+done
+
+
+
