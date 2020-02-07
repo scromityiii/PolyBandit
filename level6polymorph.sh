633e8197ae8a6442543d7e583259e49273aae86b
Steve Cromity III <s.cromityiii@gmail.com>
2020-02-06 23:21:52 -0800

    adding level6 changes

--- level6polymorph.sh
+++ level6polymorph.sh
@@ -1746,7 +1746,7 @@ if [ "$changer" == "1" ]
         #specified_user=$[RANDOM%10];
 #password_spot=$[RANDOM%10];
 
-declare -a LETTERS=("a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z" "A" "B" "C" "D" "E" "F" "G" "H" "I" "J" "K" "L" "M" "N" "O" "P" "Q" "R" "S" "T" "U" "V" "W" "X" "Y" "Z" "1" "2" "3" "4" "5" "6" "7" "8" "9" "0");
+#declare -a LETTERS=("a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z" "A" "B" "C" "D" "E" "F" "G" "H" "I" "J" "K" "L" "M" "N" "O" "P" "Q" "R" "S" "T" "U" "V" "W" "X" "Y" "Z" "1" "2" "3" "4" "5" "6" "7" "8" "9" "0");
 
 user_count=0;
 while [ $user_count -lt $user_count1 ];
