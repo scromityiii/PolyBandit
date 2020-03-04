2e0d09bb12d14e98a98f0715e4f25db1b6a6d6ae
scromityiii <42040874+scromityiii@users.noreply.github.com>
2020-03-03 22:06:40 -0500

    Update level22polymorph.sh

--- level22polymorph.sh
+++ level22polymorph.sh
@@ -254,24 +254,64 @@
 		
 	
 
-			#In the level description, the byte size of the desired file is permutated according to the results of the directly above elif statement.
-			echo "******************************************************************" >> /home/level22/README.txt;
-			echo "* Welcome to PolyBandit. This is a polymorphic clone of          *" >> /home/level22/README.txt;
-			echo "* Overthewire.org's Bandit exercise. The object is to figure out *" >> /home/level22/README.txt;
-			echo "* what the password is for the next level, then log into that    *" >> /home/level22/README.txt;
-			echo "* next level's account using SSH.                                *" >> /home/level22/README.txt;
-			echo "*  For researchers: MD5 Hash first character: "$changer"         *" >> /home/level22/README.txt;
-			echo "* You are at Level 22                                            *" >> /home/level22/README.txt;
-			echo "*                                                                *" >> /home/level22/README.txt;
-			echo "* The password for the next level is in a file with byte size "$filesize" +/-1 *" >> /home/level22/README.txt;
-			echo "*                                                                *" >> /home/level22/README.txt;
-			echo "* When you get the password for the next level, log in to the    *" >> /home/level22/README.txt;
-			echo "* next level with the command:                                   *" >> /home/level22/README.txt;
-			echo "*         ssh level23@localhost                                   *" >> /home/level22/README.txt;
-			echo "*                                                                *" >> /home/level22/README.txt;
-			echo "******************************************************************" >> /home/level22/README.txt;
+#In the level description, the byte size of the desired file is permutated according to the results of the directly above elif statement.
+echo "******************************************************************" >> /home/level22/README.txt;
+echo "* Welcome to PolyBandit. This is a polymorphic clone of          *" >> /home/level22/README.txt;
+echo "* Overthewire.org's Bandit exercise. The object is to figure out *" >> /home/level22/README.txt;
+echo "* what the password is for the next level, then log into that    *" >> /home/level22/README.txt;
+echo "* next level's account using SSH.                                *" >> /home/level22/README.txt;
+echo "*  For researchers: MD5 Hash first character: "$changer"         *" >> /home/level22/README.txt;
+echo "* You are at Level 22                                            *" >> /home/level22/README.txt;
+echo "*                                                                *" >> /home/level22/README.txt;
+echo "* The password for the next level is in a file with byte size "$filesize" +/-1 *" >> /home/level22/README.txt;
+echo "*                                                                *" >> /home/level22/README.txt;
+echo "* When you get the password for the next level, log in to the    *" >> /home/level22/README.txt;
+echo "* next level with the command:                                   *" >> /home/level22/README.txt;
+echo "*         ssh level23@localhost                                   *" >> /home/level22/README.txt;
+echo "*                                                                *" >> /home/level22/README.txt;
+echo "******************************************************************" >> /home/level22/README.txt;
 			
 		
 	
 
-			echo "cat /home/level22/README.txt" >> /home/level22/.bashrc
+echo "cat /home/level22/README.txt" >> /home/level22/.bashrc
+
+#chown level0: /home/level1;
+#chmod -rwxr-x--- /home/level1;
+#This block prevents the host system's user (the one that executes PolyBandit) as well as anybody other than the level itself from being able to read into 
+#this level's directory and its subdirectories. In essence, no cheating, you must play the game in order, and you cannot tamper with any game files unless they are in
+#the level you are currently in. The first for loop sets permissions on users before level22. The second sets them on all users after. None except level22 will be able to view the contents of /home/level22
+#until they have ssh'd into it properly. Permissions are set to block others from reading and writing to level22.
+
+setfacl -m u:level22:rwx /home/level22;
+
+
+
+for i in {0..21};
+do
+
+levelname="level"
+level="${levelname}${i}"
+
+find /home/level22 -type f -exec setfacl -m u:$level:--x {} \;
+find /home/level22 -type d -exec setfacl -m u:$level:--x {} \;
+
+(($i+1));
+done
+
+for i in {23..101};
+do
+
+levelname="level"
+level="${levelname}${i}"
+
+find /home/level22 -type f -exec setfacl -m u:$level:--x {} \;
+find /home/level22 -type d -exec setfacl -m u:$level:--x {} \;
+
+(($i+1));
+done
+
+
+setfacl -m u:$USER:--x /home/level22;	
+
+	
