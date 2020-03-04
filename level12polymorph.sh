d0cc7a2694bd9fbafb32ee0c1fcdb70a858dab44
scromityiii <42040874+scromityiii@users.noreply.github.com>
2020-03-03 21:34:03 -0500

    Update level12polymorph.sh

--- level12polymorph.sh
+++ level12polymorph.sh
@@ -259,4 +259,41 @@
 	
 
 		echo "cat /home/level12/README.txt" >> /home/level12/.bashrc
+		
+#chown level0: /home/level1;
+#chmod -rwxr-x--- /home/level1;
+#This block prevents the host system's user (the one that executes PolyBandit) as well as anybody other than the level itself from being able to read into 
+#this level's directory and its subdirectories. In essence, no cheating, you must play the game in order, and you cannot tamper with any game files unless they are in
+#the level you are currently in. The first for loop sets permissions on users before level12. The second sets them on all users after. None except level12 will be able to view the contents of /home/level12
+#until they have ssh'd into it properly. Permissions are set to block others from reading and writing to level12.
 
+setfacl -m u:level12:rwx /home/level12;
+
+
+
+for i in {0..11};
+do
+
+levelname="level"
+level="${levelname}${i}"
+
+find /home/level12 -type f -exec setfacl -m u:$level:--x {} \;
+find /home/level12 -type d -exec setfacl -m u:$level:--x {} \;
+
+(($i+1));
+done
+
+for i in {13..101};
+do
+
+levelname="level"
+level="${levelname}${i}"
+
+find /home/level12 -type f -exec setfacl -m u:$level:--x {} \;
+find /home/level12 -type d -exec setfacl -m u:$level:--x {} \;
+
+(($i+1));
+done
+
+
+setfacl -m u:$USER:--x /home/level12;
